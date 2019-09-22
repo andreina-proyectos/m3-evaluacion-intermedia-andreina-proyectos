44e178c9be23a7bab6a16092c0697fce289d7a11
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-09-22 23:34:15 +0200

    Add docs

---
+++ docs/precache-manifest.0b6d23749afdcf43c516f93613d1a94b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576172893300f3fb03c332335811a476",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e82d09624ba4ed692fce",
+    "url": "./static/css/main.805e4a0c.chunk.css"
+  },
+  {
+    "revision": "c45295ee51e149ab2fc2",
+    "url": "./static/js/2.cbb2a63c.chunk.js"
+  },
+  {
+    "revision": "e82d09624ba4ed692fce",
+    "url": "./static/js/main.6f9c299f.chunk.js"
+  },
+  {
+    "revision": "7535b7889afaa91555fb",
+    "url": "./static/js/runtime~main.2abdee5d.js"
+  }
+]);
